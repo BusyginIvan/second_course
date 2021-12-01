f5c45fa9e8ad0aada15bedb005e0eef1af6b3498
Ivan_Busygin <busygin_02@list.ru>
2021-12-01 14:35:29 +0300

    Сделал два поля в бине статическими, чтобы синхронизировать разных пользователей.

--- web-programming/lab_3/src/java/Points.java
+++ web-programming/lab_3/src/java/Points.java
@@ -5,29 +5,26 @@ import javax.sql.DataSource;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 @ManagedBean
 public class Points implements Serializable {
-    private final List<Point> pointList;
-    private Database database;
-    private final Lock lock;
-    private Point newPoint;
+    private static final List<Point> pointList = new ArrayList<>();
+    private static Database database = null;
+
+    private Point newPoint = new Point();
     private String message;
 
     @Resource(name="jdbc/datasource")
     public void setDatabase(DataSource dataSource) {
-        try {
-            database = new Database(dataSource);
-            database.loadPoints(pointList::add);
-        } catch (Exception e) { message = e.getMessage(); }
-    }
-
-    public Points() {
-        lock = new ReentrantLock();
-        pointList = new ArrayList<>();
-        newPoint = new Point();
+        synchronized (pointList) {
+            if (database == null)
+                try {
+                    database = new Database(dataSource);
+                    database.loadPoints(pointList::add);
+                } catch (Exception e) {
+                    message = e.getMessage();
+                }
+        }
     }
 
     @PreDestroy
@@ -38,16 +35,16 @@ public class Points implements Serializable {
     public String getMessage() { return message; }
 
     private void action(AutoCloseable databaseAction, Runnable localAction) {
-        if (database == null) return;
-        lock.lock();
-        try { databaseAction.close(); }
-        catch (Exception e) {
-            message = e.getMessage();
-            lock.unlock();
-            return;
+        synchronized (pointList) {
+            if (database == null) return;
+            try {
+                databaseAction.close();
+            } catch (Exception e) {
+                message = e.getMessage();
+                return;
+            }
+            localAction.run();
         }
-        localAction.run();
-        lock.unlock();
     }
 
     public void addPoint() {
