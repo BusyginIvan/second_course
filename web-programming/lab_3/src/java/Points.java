869781e7f9dd619f6d554e7d3570a463bcb50221
Ivan_Busygin <busygin_02@list.ru>
2021-12-02 09:55:33 +0300

    database не должен быть статическим.

--- web-programming/lab_3/src/java/Points.java
+++ web-programming/lab_3/src/java/Points.java
@@ -9,22 +9,19 @@ import java.util.List;
 @ManagedBean
 public class Points implements Serializable {
     private static final List<Point> pointList = new ArrayList<>();
-    private static Database database = null;
     private static final Object LOCKER = new Object();
 
+    private Database database = null;
     private Point newPoint = new Point();
     private String message;
 
     @Resource(name="jdbc/datasource")
     public void setDatabase(DataSource dataSource) {
-        synchronized (LOCKER) {
-            if (database == null)
-                try {
-                    database = new Database(dataSource);
-                    database.loadPoints(pointList::add);
-                } catch (Exception e) {
-                    message = e.getMessage();
-                }
+        try {
+            database = new Database(dataSource);
+            database.loadPoints(pointList::add);
+        } catch (Exception e) {
+            message = e.getMessage();
         }
     }
 
@@ -36,8 +33,8 @@ public class Points implements Serializable {
     public String getMessage() { return message; }
 
     private void action(AutoCloseable databaseAction, Runnable localAction) {
+        if (database == null) return;
         synchronized (LOCKER) {
-            if (database == null) return;
             try {
                 databaseAction.close();
             } catch (Exception e) {
