0a78b787c735babcf401799856f0565ef881b86e
Ivan_Busygin <busygin_02@list.ru>
2022-06-06 20:12:34 +0300

    updated computational_mathematics/lab6

--- computational_mathematics/lab6/program.py
+++ computational_mathematics/lab6/program.py
@@ -36,6 +36,16 @@ except Exception:
   print('Ошибка! Надо было ввести число, а вы ввели какую-то ерунду.')
   finish()
 
+n = input('\nВведите число интервалов разбиения (больше трёх): ')
+try:
+  n = int(n)
+except Exception:
+  print('Ошибка! Надо было ввести натуральное число, а вы ввели какую-то ерунду.')
+  finish()
+if n <= 3:
+  print('Ошибка! Ожидалось натуральное число больше трёх.')
+  finish()
+
 accuracy = input('\nЗадайте точность вычислений приближёнными методами (положительное число): ')
 try:
   accuracy = float(accuracy)
@@ -46,72 +56,65 @@ if accuracy <= 0:
   print('Ошибка! Ожидалось положительное число.')
   finish()
 
+
 f = lambda x, y: x + y
-
-
-nToH = lambda n: (b - a) / (n - 1)
+h = (b - a) / (n - 1)
+X = np.linspace(a, b, n)
 
 
 C = (y0 + a + 1) / math.exp(a)
 exactSolution = lambda x: C * math.exp(x) - x - 1
+exactSolutionY = [exactSolution(x) for x in X]
+exactSolutionDetailedX = np.linspace(a, b, 200)
+exactSolutionDetailedY = [exactSolution(x) for x in exactSolutionDetailedX]
 
-exactSolutionN = 200
-exactSolutionX = np.linspace(a, b, exactSolutionN)
-exactSolutionY = [exactSolution(x) for x in exactSolutionX]
 
+def approximateSolution(nextY, degreeOfAccuracy):
+  Y = [y0]; detailedX = [a]; detailedY = [y0]
+  for i in range(n - 1):
+    subN = 2
+    oldY = nextY(X[i], Y[i], h)
+    while True:
+      subH = h / subN
+      subX = [X[i]]; subY = [Y[i]]
+      for j in range(subN):
+        subX.append(subX[-1] + subH)
+        subY.append(nextY(subX[-1], subY[-1], subH))
+      newY = subY[-1]
+      if abs(newY - oldY) / (2**degreeOfAccuracy - 1) < accuracy: break
+      oldY = newY; subN *= 2
+    detailedX.extend(subX[1:])
+    detailedY.extend(subY[1:])
+    Y.append(newY)
+  return (Y, detailedX, detailedY)
 
 eulerNextY = lambda x, y, h: y + h / 2 * (f(x, y) + f(x + h, y + h * f(x, y)))
+eulerY, eulerDetailedX, eulerDetailedY = approximateSolution(eulerNextY, 2)
 
-eulerN = 3; eulerH = nToH(eulerN)
-eulerDegreeOfAccuracy = 2
-while abs(eulerNextY(a + eulerH, eulerNextY(a, y0, eulerH), eulerH) - eulerNextY(a, y0, 2 * eulerH)) / (2**eulerDegreeOfAccuracy - 1) > accuracy:
-  eulerN *= 2; eulerH = nToH(eulerN)
-print('\n\nДля достижения заданной точности в методе Эйлера будет использоваться интервал разбиения', eulerH, '\n')
+def rungeNextY(x, y, h):
+  k1 = f(x, y); k2 = f(x + h/2, y + h/2 * k1); k3 = f(x + h/2, y + h/2 * k2); k4 = f(x + h, y + h * k3)
+  return y + h/6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
-eulerX = np.linspace(a, b, eulerN)
-eulerY = [y0]
-for i in range(1, eulerN):
-  eulerY.append(eulerNextY(eulerX[i - 1], eulerY[i - 1], eulerH))
+rungeY, rungeDetailedX, rungeDetailedY = approximateSolution(rungeNextY, 4)
 
-exactSolutionYForEulerX = [exactSolution(x) for x in eulerX]
-print('   i          x          Метод Эйлера   Точное решение')
-for i in range(eulerN):
-  print(' %5d  %14.5f  %14.5f  %14.5f ' % (i, eulerX[i], eulerY[i], exactSolutionYForEulerX[i]))
+
+#     ' .....  ..............  ..............  .................   ..............'
+print('   i          x          Метод Эйлера   Метод Рунге-Кутта   Точное решение')
+for i in range(n):
+  print(' %5d  %13.5f   %13.5f   %15.5f    %13.5f' % (i, X[i], eulerY[i], rungeY[i], exactSolutionY[i]))
 print('\n')
-
-
-def adamsNextY(x0, y0, y1, y2, y3, h):
-  f0 = f(x0, y0); f1 = f(x0 + h, y1); f2 = f(x0 + 2 * h, y2); f3 = f(x0 + 3 * h, y3)
-  return y3 + h / 24 * (-9 * f0 + 37 * f1 - 59 * f2 + 55 * f3)
-
-adamsN = 5; adamsH = nToH(adamsN)
-adamsDegreeOfAccuracy = 4
-while abs(eulerNextY(a + adamsH, eulerNextY(a, y0, adamsH), adamsH) - eulerNextY(a, y0, 2 * adamsH)) / (2**adamsDegreeOfAccuracy - 1) > accuracy:
-  adamsN *= 2; adamsH = nToH(adamsN)
-if adamsN == 5:
-  print('Метод Адамса имеет смысл при четырёх интервалах в разбиении и более, в данном случае нужная точность достигается уже при четырёх интервалах, так что один интервал разбиения будет равен', adamsH)
-else:
-  print('В методе Адамса будет использоваться интервал разбиения', adamsH)
-print()
-
-adamsX = np.linspace(a, b, adamsN)
-adamsY = [y0]
-for i in range(1, 4):
-  adamsY.append(eulerNextY(adamsX[i - 1], adamsY[i - 1], adamsH))
-for i in range(4, adamsN):
-  adamsY.append(adamsNextY(adamsX[i - 4], adamsY[i - 4], adamsY[i - 3], adamsY[i - 2], adamsY[i - 1], adamsH))
-
-exactSolutionYForAdamsX = [exactSolution(x) for x in adamsX]
-print('   i          x          Метод Адамса   Точное решение')
-for i in range(adamsN):
-  print(' %5d  %14.5f  %14.5f  %14.5f ' % (i, adamsX[i], adamsY[i], exactSolutionYForAdamsX[i]))
 
 
 _, ax = plt.subplots(figsize=(8, 5), label = 'Моё окно')
 
-ax.plot(exactSolutionX, exactSolutionY, '--', label = 'точное решение', alpha = 0.8, zorder = 2, color = 'black')
-ax.plot(eulerX        , eulerY        , '-' , label = 'метод Эйлера'  , alpha = 0.8, zorder = 1, color = 'red'  )
-ax.plot(adamsX        , adamsY        , '-' , label = 'метод Адамса'  , alpha = 0.8, zorder = 1, color = 'blue' )
+ax.plot(eulerDetailedX        , eulerDetailedY        , '-' , label = 'метод Эйлера'     , alpha = 0.7, color = 'red'  )
+ax.plot(rungeDetailedX        , rungeDetailedY        , '-' , label = 'метод Рунге-Кутта', alpha = 0.7, color = 'blue' )
+ax.plot(exactSolutionDetailedX, exactSolutionDetailedY, '--', label = 'точное решение'   , alpha = 1.0, color = 'black')
+
+for i in range(n):
+  ax.plot(X[i], eulerY[i]        , marker = 'o', fillstyle = 'full', alpha = 0.8, markerfacecolor = 'red'  , markeredgecolor = 'red'  )
+  ax.plot(X[i], rungeY[i]        , marker = 'o', fillstyle = 'full', alpha = 0.8, markerfacecolor = 'blue' , markeredgecolor = 'blue' )
+  ax.plot(X[i], exactSolutionY[i], marker = 'o', fillstyle = 'full', alpha = 0.8, markerfacecolor = 'white', markeredgecolor = 'black')
 
 ax.grid(True)
 ax.set_title('Прекрасный график')
